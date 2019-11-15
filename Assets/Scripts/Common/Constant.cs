413320245fede4604eaa9ebaae7bbdf40edce2d7
Myeong <juharim@naver.com>
2019-11-15 10:08:53 +0900

    수정

--- Assets/Scripts/Common/Constant.cs
+++
@@ -1,7 +0,0 @@
-﻿public class Constant
-{
-    public const string kFileName = "data.json";
-    public const string kIsFirst = "FirstPopup";
-    public const string kViewManagerOpen = "open";
-    public const string kViewManagerClose = "close";
-}
